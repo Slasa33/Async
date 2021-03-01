8441dcb257537bc5d34d5a670b62d3e1408743b3
Jakub Jancicka <jan0751@vsb.cz>
2021-03-01 18:01:37 +0100

    Add project files.

---
+++ Async/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Experiment();
+        }
+
+        public static async Task Experiment()
+        {
+            Console.WriteLine("Start");
+            await Task.Delay(1000);
+
+            string file = @"sample.txt";
+            using (FileStream stream = new FileStream(file, FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (StreamWriter stramwriter = new StreamWriter(stream))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        await stramwriter.WriteLineAsync(i.ToString());
+                    }
+                }
+            }
+            await Task.Delay(1000);
+            Console.WriteLine("end");
+        }
+    }
+}
